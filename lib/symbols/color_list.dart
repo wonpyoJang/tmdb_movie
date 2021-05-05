0faa57dee17eeee628a023e92b339cdd2062a85e
Wonpyo Jang <jangwonpyo0@gmail.com>
2021-05-05 21:58:44 +0900

    [added] 색상 추가, lightGrey

--- lib/symbols/color_list.dart
+++ lib/symbols/color_list.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 class ColorList {
   static const Color black = const Color(0xff000000);
   static const Color shadow = const Color(0x40000000);
+  static const Color lightGrey = const Color(0xff828282);
   static const Color grey = const Color(0xff9a9a9a);
   static const Color strongGrey = const Color(0xffB9B9B9);
   static const Color red = const Color(0xfff66060);
