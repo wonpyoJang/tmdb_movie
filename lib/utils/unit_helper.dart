16323ea94b967bf8fe948ed263f14623c0189d17
Wonpyo Jang <jangwonpyo@jetserve.net>
2021-05-05 16:18:26 +0900

    [added] UnitHelper, 텍스트 크기를 픽셀에서 비율로 변환하는 함수 추가

--- lib/utils/unit_helper.dart
+++ lib/utils/unit_helper.dart
@@ -2,4 +2,8 @@ class UnitHelper {
   static emToPixel(double em) {
     return em / 16;
   }
+
+  static getTextHeightRatio(double heightInPixel, double fontSizeInPixel) {
+    return heightInPixel / fontSizeInPixel;
+  }
 }
