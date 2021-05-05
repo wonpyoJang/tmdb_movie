4fa5dc8a1a8ef4c224920faec00a5f9eb7a546dc
Wonpyo Jang <jangwonpyo0@gmail.com>
2021-05-05 16:35:35 +0900

    [fixed] dart sdk 2.12.0 이상 사용하도록 변경

--- lib/utils/unit_helper.dart
+++ lib/utils/unit_helper.dart
@@ -1,9 +1,9 @@
 class UnitHelper {
-  static emToPixel(double em) {
+  static getPixel({required double em}) {
     return em / 16;
   }
 
-  static getTextHeightRatio(double heightInPixel, double fontSizeInPixel) {
+  static getTextHeightRatio({required double heightInPixel, required double fontSizeInPixel}) {
     return heightInPixel / fontSizeInPixel;
   }
 }
